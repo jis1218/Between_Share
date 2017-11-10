91924989c295dacdf705439223ea7897e1361b8f
Anya <choi.bosel@gmail.com>
2017-11-10 22:04:54 +0900

    longclick_picture

--- app/src/main/java/com/project/between/AnniversaryAndCalendar/HomeActivity.java
+++ app/src/main/java/com/project/between/AnniversaryAndCalendar/HomeActivity.java
@@ -1,17 +1,26 @@
 package com.project.between.AnniversaryAndCalendar;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.project.between.R;
 import com.project.between.chatting.ChattingActivity;
 import com.project.between.more.MoreActivity;
+
+import java.io.IOException;
 
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
@@ -22,7 +31,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     ImageButton btnCommonHome;
     ImageButton btnCommonChatting;
     ImageButton btnCommonMore;
-
+    ImageView ivBackground;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +39,14 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         initView();
         initListener();
+        initBackground();
 //        initFragment();
     }
+
+    private void initBackground() {
+        ivBackground=(ImageView)findViewById(R.id.imageView);
+    }
+
     public void initFragment(){
         Log.e("---------Activity----","  [Add] ListFragment()-----");
 //        getSupportFragmentManager()
@@ -60,6 +75,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         btnCommonChatting    = (ImageButton) findViewById(R.id.btnCommonChatting);
     }
 
+    public static final int SELECT_IMAGE=1003;
     @Override
     public void onClick(View view) {
         int id=view.getId();
@@ -85,6 +101,43 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnCommonChatting:
                 startActivity(new Intent(HomeActivity.this, ChattingActivity.class));
                 break;
+            case R.id.imageView:
+                Intent i = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(i, SELECT_IMAGE);
+                break;
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_IMAGE)
+        {
+            if (resultCode == Activity.RESULT_OK)
+            {
+                if (data != null) {
+                    try {
+                        Uri selectedImage = data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                        Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                        cursor.moveToFirst();
+
+                        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                        String filePath = cursor.getString(columnIndex);
+                        cursor.close();
+
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
+                        ivBackground.setImageBitmap(bitmap);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED)
+            {
+                Toast.makeText(getApplicationContext(), "Cancelled", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
